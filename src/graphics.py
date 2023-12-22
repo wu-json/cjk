4445cc3ed1c53d2e5ee6baafef801afd73195d2f
wu-json <jason.wu2153@gmail.com>
2023-12-22 16:18:10 -0500

    feat: cleanup

--- src/graphics.py
+++ src/graphics.py
@@ -14,3 +14,14 @@ def print_line(thick=False):
     width = os.get_terminal_size().columns
     char = "=" if thick else "-"
     print(char * width)
+
+
+def print_welcome_message():
+    """Prints the welcome message when CJK is first opened."""
+    print_line(thick=True)
+    print(CJK_ASCII_ART)
+    print_line(thick=True)
+    print(
+        "Welcome to CJK! This is an LLM-powered command line tool for learning words in 中文,日本語、and 한국어 all at once!"
+    )
+    print_line()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,4 @@
-from graphics import CJK_ASCII_ART, print_line
-
-
-def print_welcome_message():
-    """Prints the welcome message when CJK is first opened."""
-    print_line(thick=True)
-    print(CJK_ASCII_ART)
-    print_line(thick=True)
-    print(
-        "Welcome to CJK! This is an LLM-powered command line tool for learning words in 中文,日本語、and 한국어 all at once!"
-    )
-    print_line()
+from graphics import print_welcome_message
 
 
 def main():
