271c9d9411eeb22e552c75393e6de58b5376dc28
wu-json <jason.wu2153@gmail.com>
2023-12-22 15:28:54 -0500

    feat: gpt4all hello world

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from gpt4all import GPT4All
+
+
 def main():
-    print("Hello, World!")
+    model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
+    output = model.generate("Who is Ichigo Kurosaki", max_tokens=30)
+    print(output)
 
 
 if __name__ == "__main__":
