5e895fb1bef1adb8928e2f75d9148e7bf9286824
wu-json <jason.wu2153@gmail.com>
2023-12-23 10:49:51 -0500

    feat: update prompt

--- src/prompts.py
+++ src/prompts.py
@@ -6,6 +6,7 @@ model = GPT4All("mistral-7b-instruct-v0.1.Q4_0.gguf")
 def get_translated_word(word: str, language: str):
     return model.generate(
         f"""
-        [INST]translate "{word}" to {language}, and give an example sentence using "{word}" in {language}.[/INST]
-        """
+        [INST]translate "{word}" to {language}, and give an example sentence using "{word}" in {language}. include english translation of the example sentence.[/INST]
+        """,
+        max_tokens=500,
     )
